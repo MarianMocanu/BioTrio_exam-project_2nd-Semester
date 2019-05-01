56a20d104479e4e146c5a9f22337bc3fdcf21674
Marian <gheo0174@stud.kea.dk>
2019-05-01 21:05:28 +0200

    Added ticket and booking repositories
    all code nicely reformatted

---
+++ src/main/java/dk/kea/stud/biotrio/ticketing/BookingRepository.java
@@ -0,0 +1,67 @@
+package dk.kea.stud.biotrio.ticketing;
+
+import dk.kea.stud.biotrio.cinema.ScreeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookingRepository {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+    @Autowired
+    private ScreeningRepository screeningRepo;
+
+    public List<Booking> findBookingsForScreening(int id) {
+        Booking booking = null;
+        List<Booking> screeningBookings = new ArrayList<>();
+
+        String query = "SELECT * FROM bookings WHERE screening_id = ?";
+        SqlRowSet bookingsRS = jdbc.queryForRowSet( query, id );
+
+        //iteration for setting the bookings received from the database
+        while (bookingsRS.next()) {
+            booking = new Booking();
+            booking.setId( bookingsRS.getInt( "id" ) );
+            booking.setPhoneNo( bookingsRS.getString( "phone_no" ) );
+            booking.setCode( bookingsRS.getString( "code" ) );
+            booking.setScreening( screeningRepo.findById( bookingsRS.getInt( "screening_id" ) ) );
+
+            //Booking object has an array of Seat objects
+            Seat seat = null;
+            List bookingSeats = new ArrayList<>();
+
+            String queryForSeats = "SELECT * FROM booked_seats WHERE booking_id = ?";
+            SqlRowSet seatsRS = jdbc.queryForRowSet( queryForSeats, booking.getId() );
+
+            //iteration for setting the seats received from database
+            while (seatsRS.next()) {
+                seat = new Seat();
+                seat.setRowNo( seatsRS.getInt( "row_no" ) );
+                seat.setSeatNo( seatsRS.getInt( "seat_no" ) );
+
+                //adding the booked seat in the array
+                bookingSeats.add( seat );
+            }
+
+            //setting the last attribute of the Booking object
+            booking.setSeats( bookingSeats );
+
+            //adding the Booking object in the screenings Array
+            screeningBookings.add( booking );
+        }
+        return screeningBookings;
+    }
+
+    //TODO public Booking addBooking(Booking booking) {}
+
+    //TODO public void updateBooking(Booking booking) {}
+
+    //TODO public void deleteBooking(int id) {}
+
+}
---
+++ src/main/java/dk/kea/stud/biotrio/ticketing/TicketRepository.java
@@ -0,0 +1,84 @@
+package dk.kea.stud.biotrio.ticketing;
+
+import dk.kea.stud.biotrio.cinema.ScreeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TicketRepository {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+    @Autowired
+    private ScreeningRepository screeningRepo;
+
+    public List<Ticket> findTicketsForScreening(int id) {
+        Ticket ticket = null;
+        List<Ticket> screeningTickets = new ArrayList<>();
+
+        String query = "SELECT * FROM tickets WHERE screening_id = ?";
+        SqlRowSet rs = jdbc.queryForRowSet( query, id );
+
+        while (rs.next()) {
+            ticket = new Ticket();
+            ticket.setId( rs.getInt( "id" ) );
+            ticket.setScreening( screeningRepo.findById( rs.getInt( "screening_id" ) ) );
+            ticket.getSeat().setRowNo( rs.getInt( "row_no" ) );
+            ticket.getSeat().setSeatNo( rs.getInt( "seat_no" ) );
+
+            screeningTickets.add( ticket );
+        }
+        return screeningTickets;
+    }
+
+    public Ticket addTicket(Ticket ticket) {
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(
+                        "INSERT INTO tickets (screening_id, row_no, seat_no) " +
+                                "VALUES (?, ?, ?)", new String[]{"id"}
+                );
+                ps.setInt( 1, ticket.getScreening().getId() );
+                ps.setInt( 2, ticket.getSeat().getRowNo() );
+                ps.setInt( 3, ticket.getSeat().getSeatNo() );
+                return ps;
+            }
+        };
+
+        KeyHolder key = new GeneratedKeyHolder();
+        jdbc.update( psc, key );
+
+        ticket.setId( key.getKey().intValue() );
+        return ticket;
+    }
+
+    public void updateTicket(Ticket ticket) {
+        String query = "UPDATE tickets SET " +
+                "screening_id = ?, " +
+                "row_no = ?, " +
+                "seat_no = ? " +
+                "WHERE id = ?;";
+        jdbc.update( query,
+                ticket.getScreening().getId(),
+                ticket.getSeat().getRowNo(),
+                ticket.getSeat().getSeatNo(),
+                ticket.getId() );
+    }
+
+    public void deleteTicket(int id) {
+        jdbc.update( "DELETE FROM tickets WHERE id = ?;", id );
+    }
+
+}
