47f6df9a76377323d05e894d5a5959c850f38413
Marian <gheo0174@stud.kea.dk>
2019-05-01 14:13:11 +0200

    CRUD Movie - retouched

--- src/main/java/dk/kea/stud/biotrio/cinema/MovieController.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -13,13 +14,12 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepo;
 
+    //not necessary so far
     @GetMapping("/movies")
     public String findMovies(Model model){
         model.addAttribute( "movies", movieRepo.findAllMovies() );
         return "movies/movies-view";
     }
-
-    //TODO @GetMapping("/manage")
 
      @GetMapping("/manage/movies")
     public String manageMovies(Model model){
@@ -39,10 +39,22 @@ public class MovieController {
         return "redirect:/manage/movies";
     }
 
-    //TODO @GetMapping("/manage/movies/edit/{id}")
+    @GetMapping("/manage/movies/edit/{id}")
+    public String editMovie(@PathVariable("id") int id, Model model) {
+        model.addAttribute( "currentMovie", movieRepo.findMovieById(id) );
+        return "movies/edit-movie";
+    }
 
-    //TODO @GetMapping("manage/movies/delete/{id}")
+    @PostMapping("/manage/movies/edit")
+    public String updateMovie( @ModelAttribute Movie movie) {
+        movieRepo.updateMovie( movie );
+        return "redirect:/manage/movies";
+    }
 
-
+    @GetMapping("/manage/movies/delete/{id}")
+    public String deleteMovie(@PathVariable("id") int id) {
+        movieRepo.deleteMovie( id );
+        return "redirect:/manage/movies";
+    }
 
 }
--- src/main/java/dk/kea/stud/biotrio/cinema/MovieRepository.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/MovieRepository.java
@@ -8,10 +8,7 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +44,30 @@ public class MovieRepository {
         return moviesList;
     }
 
-    public int addMovie(Movie movie) {
+    public Movie findMovieById(int id) {
+        Movie movie = new Movie();
+        SqlRowSet rs = jdbc.queryForRowSet( "SELECT * FROM movies WHERE id = " + id );
+        while (rs.next()) {
+            movie.setId( rs.getInt( "id" ) );
+            movie.setTitle( rs.getString( "title" ) );
+            movie.setRuntime( rs.getInt( "runtime" ) );
+            movie.setSynopsis( rs.getString( "synopsis" ) );
+            movie.setGenre( rs.getString( "genre" ) );
+            movie.setLanguage( rs.getString( "language" ) );
+            movie.setSubtitles( rs.getString( "subtitles" ) );
+            movie.setProjectionType( rs.getString( "projection_type" ) );
+            movie.setTrailerLink( rs.getString( "trailer_link" ) );
+            Timestamp ts = rs.getTimestamp( "release_date" );
+            movie.setReleaseDate( ts == null ? null : ts.toLocalDateTime().toLocalDate() );
+            movie.setCast( rs.getString( "cast" ) );
+            movie.setDirector( rs.getString( "director" ) );
+            movie.setAgeRestriction( rs.getString( "age_restriction" ) );
+            movie.setPoster( rs.getString( "poster" ) );
+        }
+        return movie;
+    }
+
+    public Movie addMovie(Movie movie) {
         PreparedStatementCreator psc = new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -61,7 +81,44 @@ public class MovieRepository {
         };
         KeyHolder id = new GeneratedKeyHolder();
         jdbc.update( psc, id );
-
-        return id.getKey().intValue();
+        movie.setId( id.getKey().intValue() );
+        return movie;
     }
+
+    public void updateMovie(Movie movie) {
+        jdbc.update( "UPDATE movies SET " +
+                        "title = ?," +
+                        "runtime = ?," +
+                        "synopsis = ?, " +
+                        "genre = ?, " +
+                        "language = ?, " +
+                        "subtitles = ?," +
+                        "projection_type = ?," +
+                        "trailer_link = ?," +
+                        "release_date = ?," +
+                        "cast = ?," +
+                        "director = ?," +
+                        "age_restriction = ?," +
+                        "poster = ? " +
+                        "WHERE id= ?",
+                movie.getTitle(),
+                movie.getRuntime(),
+                movie.getSynopsis(),
+                movie.getGenre(),
+                movie.getLanguage(),
+                movie.getSubtitles(),
+                movie.getProjectionType(),
+                movie.getTrailerLink(),
+                movie.getReleaseDate() == null ? null : Date.valueOf( movie.getReleaseDate() ),
+                movie.getCast(),
+                movie.getDirector(),
+                movie.getAgeRestriction(),
+                movie.getPoster(),
+                movie.getId() );
+    }
+
+    public void deleteMovie(int id) {
+        jdbc.update( "DELETE FROM movies WHERE id = ?", id );
+    }
+
 }
