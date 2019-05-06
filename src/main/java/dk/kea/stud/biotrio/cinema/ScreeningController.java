d79f8c634e963b94904d2000762cafdcf9da135a
Chris Szabo <szabo.krisztian.87@gmail.com>
2019-05-06 19:44:32 +0200

    Field errors on add screening!
    This is the way Alex suggested to do things, I want to show him that it throws and error in the way I did it; perhaps he can spot the problem

--- src/main/java/dk/kea/stud/biotrio/cinema/ScreeningController.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/ScreeningController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
@@ -21,34 +22,61 @@ public class ScreeningController {
 
   // For the end-users
   @GetMapping("/screenings")
-  public String screenings(Model model) {
+  public String screeningsUsers(Model model) {
     model.addAttribute("upcomingScreenings", screeningRepo.findUpcomingScreenings());
-    return "screenings/screenings-view";
+    return "screenings/user/screenings-view";
   }
 
   // For the employees
+  @GetMapping("/manage/screenings")
+  public String screenings(Model model) {
+    model.addAttribute("screenings", screeningRepo.findAllScreenings());
+    return "screenings/screenings-view";
+  }
+
   @GetMapping("/manage/screenings/add")
   public String manageScreenings(Model model) {
     model.addAttribute("movies", movieRepo.findAllMovies());
     model.addAttribute("theaters", theaterRepo.findAllTheaters());
-    model.addAttribute("screeningForm", new ScreeningForm());
+    model.addAttribute("screening", new Screening());
     return "screenings/screenings-add";
   }
 
   @PostMapping("/manage/screenings/save")
-  public String addScreening(@ModelAttribute ScreeningForm screeningForm) {
-    Screening screening = new Screening();
-    screening.setMovie(movieRepo.findMovieById(screeningForm.getMovieId()));
-    screening.setTheater(theaterRepo.findTheater(screeningForm.getTheaterId()));
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    LocalDateTime startTime;
-    try {
-       startTime = LocalDateTime.parse(screeningForm.getStartTime(), formatter);
-    } catch (Exception e) {
-      return "redirect:/manage/screenings/add?start_time_error";
-    }
-    screening.setStartTime(startTime);
+  public String addScreening(@ModelAttribute Screening screening) {
+//    Screening screening = new Screening();
+//    screening.setMovie(movieRepo.findMovieById(screeningForm.getMovieId()));
+//    screening.setTheater(theaterRepo.findTheater(screeningForm.getTheaterId()));
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+//    LocalDateTime startTime;
+//    try {
+//       startTime = LocalDateTime.parse(screeningForm.getStartTime(), formatter);
+//    } catch (Exception e) {
+//      return "redirect:/manage/screenings/add?start_time_error";
+//    }
+//    screening.setStartTime(startTime);
+    System.out.println(screening);
     screeningRepo.addScreening(screening);
-    return "redirect:/screenings";
+    return "redirect:/manage/screenings";
+  }
+
+  @GetMapping("/manage/screenings/edit/{id}")
+  public String editScreening(@PathVariable("id") int id, Model model) {
+    model.addAttribute("selectedScreening", screeningRepo.findById(id));
+    model.addAttribute("movies", movieRepo.findAllMovies());
+    model.addAttribute("theaters", theaterRepo.findAllTheaters());
+    return "screenings/screenings-edit";
+  }
+
+  @PostMapping("/manage/screenings/edit")
+  public String commitEdit(@ModelAttribute Screening screening) {
+    screeningRepo.updateScreening(screening);
+    return "redirect:/manage/screenings";
+  }
+
+  @GetMapping("/manage/screenings/delete/{id}")
+  public String deleteScreening(@PathVariable("id") int id) {
+    screeningRepo.deleteScreening(id);
+    return "redirect:/manage/screenings";
   }
 }
