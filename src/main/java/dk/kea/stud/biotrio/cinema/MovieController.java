c88a2f750d25aa04e58de01882ea1fe40d6bfd9c
Malgo0102 <mal.wit@hotmail.com>
2019-05-11 08:54:49 +0200

    Movie Delete done

--- src/main/java/dk/kea/stud/biotrio/cinema/MovieController.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/MovieController.java
@@ -11,50 +11,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MovieController {
 
-    @Autowired
-    private MovieRepository movieRepo;
+  @Autowired
+  private MovieRepository movieRepo;
 
-    //not necessary so far
-    @GetMapping("/movies")
-    public String findMovies(Model model){
-        model.addAttribute( "movies", movieRepo.findAllMovies() );
-        return "movies/movies-view";
-    }
+  //not necessary so far
+  @GetMapping("/movies")
+  public String findMovies(Model model) {
+    model.addAttribute("movies", movieRepo.findAllMovies());
+    return "movies/movies-view";
+  }
 
-     @GetMapping("/manage/movies")
-    public String manageMovies(Model model){
-         model.addAttribute( "movies", movieRepo.findAllMovies() );
-        return "movies/manage-movies";
-     }
+  @GetMapping("/manage/movies")
+  public String manageMovies(Model model) {
+    model.addAttribute("movies", movieRepo.findAllMovies());
+    return "movies/manage-movies";
+  }
 
-    @GetMapping("/manage/movies/add")
-    public String addMovie(Model model) {
-        model.addAttribute( "movie", new Movie() );
-        return "movies/add-movie";
-    }
+  @GetMapping("/manage/movies/add")
+  public String addMovie(Model model) {
+    model.addAttribute("movie", new Movie());
+    return "movies/add-movie";
+  }
 
-    @PostMapping("/manage/movies/save")
-    public String saveMovie(@ModelAttribute Movie movie) {
-        movieRepo.addMovie( movie );
-        return "redirect:/manage/movies";
-    }
+  @PostMapping("/manage/movies/save")
+  public String saveMovie(@ModelAttribute Movie movie) {
+    movieRepo.addMovie(movie);
+    return "redirect:/manage/movies";
+  }
 
-    @GetMapping("/manage/movies/edit/{id}")
-    public String editMovie(@PathVariable("id") int id, Model model) {
-        model.addAttribute( "currentMovie", movieRepo.findMovieById(id) );
-        return "movies/edit-movie";
-    }
+  @GetMapping("/manage/movies/edit/{id}")
+  public String editMovie(@PathVariable("id") int id, Model model) {
+    model.addAttribute("currentMovie", movieRepo.findMovieById(id));
+    return "movies/edit-movie";
+  }
 
-    @PostMapping("/manage/movies/edit")
-    public String updateMovie( @ModelAttribute Movie movie) {
-        movieRepo.updateMovie( movie );
-        return "redirect:/manage/movies";
-    }
+  @PostMapping("/manage/movies/edit")
+  public String updateMovie(@ModelAttribute Movie movie) {
+    movieRepo.updateMovie(movie);
+    return "redirect:/manage/movies";
+  }
 
-    @GetMapping("/manage/movies/delete/{id}")
-    public String deleteMovie(@PathVariable("id") int id) {
-        movieRepo.deleteMovie( id );
-        return "redirect:/manage/movies";
-    }
+  //From "delete" button goes to "Are you sure to delete(...)?"
+  @GetMapping("/manage/movies/delete/{id}")
+  public String deleteMovie(@PathVariable(name = "id") int id, Model m) {
+    m.addAttribute("movie", movieRepo.findMovieById(id));
+
+    return "movies/movies-delete";
+  }
+
+  //Deletes Movie and lists all the Movies
+  @PostMapping("/manage/movies/delete")
+  public String deleteMovie(int id) {
+    movieRepo.deleteMovie(id);
+
+    return "redirect:/manage/movies";
+  }
 
 }
