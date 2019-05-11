4b783a99109bc6fc5db71a10386607f246efc521
Chris Szabo <szabo.krisztian.87@gmail.com>
2019-05-11 08:51:15 +0200

    Fix addMovie() to insert all data into db

--- src/main/java/dk/kea/stud/biotrio/cinema/MovieRepository.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/MovieRepository.java
@@ -15,121 +15,132 @@ import java.util.List;
 @Repository
 public class MovieRepository {
 
-  @Autowired
-  private JdbcTemplate jdbc;
+    @Autowired
+    private JdbcTemplate jdbc;
 
-  public List<Movie> findAllMovies() {
-    List<Movie> moviesList = new ArrayList<>();
+    public List<Movie> findAllMovies() {
+        List<Movie> moviesList = new ArrayList<>();
 
-    String query = "SELECT * FROM movies";
-    SqlRowSet rs = jdbc.queryForRowSet(query);
+        String query = "SELECT * FROM movies";
+        SqlRowSet rs = jdbc.queryForRowSet(query);
 
-    while (rs.next()) {
-      Movie movie = new Movie();
-      movie.setId(rs.getInt("id"));
-      movie.setTitle(rs.getString("title"));
-      movie.setRuntime(rs.getInt("runtime"));
-      movie.setSynopsis(rs.getString("synopsis"));
-      movie.setGenre(rs.getString("genre"));
-      movie.setLanguage(rs.getString("language"));
-      movie.setSubtitles(rs.getString("subtitles"));
-      movie.setProjectionType(rs.getString("projection_type"));
-      movie.setTrailerLink(rs.getString("trailer_link"));
-      Timestamp ts = rs.getTimestamp("release_date");
-      movie.setReleaseDate(ts == null ? null : ts.toLocalDateTime().toLocalDate());
-      movie.setCast(rs.getString("cast"));
-      movie.setDirector(rs.getString("director"));
-      movie.setAgeRestriction(rs.getString("age_restriction"));
-      movie.setPoster(rs.getString("poster"));
+        while (rs.next()) {
+            Movie movie = new Movie();
+            movie.setId(rs.getInt("id"));
+            movie.setTitle(rs.getString("title"));
+            movie.setRuntime(rs.getInt("runtime"));
+            movie.setSynopsis(rs.getString("synopsis"));
+            movie.setGenre(rs.getString("genre"));
+            movie.setLanguage(rs.getString("language"));
+            movie.setSubtitles(rs.getString("subtitles"));
+            movie.setProjectionType(rs.getString("projection_type"));
+            movie.setTrailerLink(rs.getString("trailer_link"));
+            Timestamp ts = rs.getTimestamp("release_date");
+            movie.setReleaseDate(ts == null ? null : ts.toLocalDateTime().toLocalDate());
+            movie.setCast(rs.getString("cast"));
+            movie.setDirector(rs.getString("director"));
+            movie.setAgeRestriction(rs.getString("age_restriction"));
+            movie.setPoster(rs.getString("poster"));
 
-      moviesList.add(movie);
+            moviesList.add(movie);
+        }
+        return moviesList;
     }
-    return moviesList;
-  }
 
-  public Movie findMovieById(int id) {
-    Movie movie = null;
+    public Movie findMovieById(int id) {
+        Movie movie = null;
 
-    String query = "SELECT * FROM movies WHERE id = ?";
-    SqlRowSet rs = jdbc.queryForRowSet(query, id);
-    if (rs.first()) {
-      movie = new Movie();
+        String query = "SELECT * FROM movies WHERE id = ?";
+        SqlRowSet rs = jdbc.queryForRowSet(query, id);
+        if (rs.first()) {
+            movie = new Movie();
 
-      movie.setId(rs.getInt("id"));
-      movie.setTitle(rs.getString("title"));
-      movie.setRuntime(rs.getInt("runtime"));
-      movie.setSynopsis(rs.getString("synopsis"));
-      movie.setGenre(rs.getString("genre"));
-      movie.setLanguage(rs.getString("language"));
-      movie.setSubtitles(rs.getString("subtitles"));
-      movie.setProjectionType(rs.getString("projection_type"));
-      movie.setTrailerLink(rs.getString("trailer_link"));
-      Timestamp ts = rs.getTimestamp("release_date");
-      movie.setReleaseDate(ts == null ? null : ts.toLocalDateTime().toLocalDate());
-      movie.setCast(rs.getString("cast"));
-      movie.setDirector(rs.getString("director"));
-      movie.setAgeRestriction(rs.getString("age_restriction"));
-      movie.setPoster(rs.getString("poster"));
+            movie.setId(rs.getInt("id"));
+            movie.setTitle(rs.getString("title"));
+            movie.setRuntime(rs.getInt("runtime"));
+            movie.setSynopsis(rs.getString("synopsis"));
+            movie.setGenre(rs.getString("genre"));
+            movie.setLanguage(rs.getString("language"));
+            movie.setSubtitles(rs.getString("subtitles"));
+            movie.setProjectionType(rs.getString("projection_type"));
+            movie.setTrailerLink(rs.getString("trailer_link"));
+            Timestamp ts = rs.getTimestamp("release_date");
+            movie.setReleaseDate(ts == null ? null : ts.toLocalDateTime().toLocalDate());
+            movie.setCast(rs.getString("cast"));
+            movie.setDirector(rs.getString("director"));
+            movie.setAgeRestriction(rs.getString("age_restriction"));
+            movie.setPoster(rs.getString("poster"));
+        }
+        return movie;
     }
-    return movie;
-  }
 
-  public Movie addMovie(Movie movie) {
-    PreparedStatementCreator psc = new PreparedStatementCreator() {
-      @Override
-      public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-        PreparedStatement ps = connection.prepareStatement(
-            "INSERT INTO movies (title, runtime, synopsis, genre) " +
-                "VALUES  (?,?,?,?);", new String[]{"id"});
-        ps.setString(1, movie.getTitle());
-        ps.setInt(2, movie.getRuntime());
-        ps.setString(3, movie.getSynopsis());
-        ps.setString(4, movie.getGenre());
-        return ps;
-      }
-    };
+    public Movie addMovie(Movie movie) {
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO movies (title, runtime, " +
+                        "synopsis, genre, language, subtitles, projection_type, trailer_link, release_date, " +
+                        "cast, director, age_restriction, poster) VALUES  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                        new String[]{"id"});
+                ps.setString(1, movie.getTitle());
+                ps.setInt(2, movie.getRuntime());
+                ps.setString(3, movie.getSynopsis());
+                ps.setString(4, movie.getGenre());
+                ps.setString(5, movie.getLanguage());
+                ps.setString(6, movie.getSubtitles());
+                ps.setString(7, movie.getProjectionType());
+                ps.setString(8, movie.getTrailerLink());
+                Date relaseDate = movie.getReleaseDate() == null ? null : Date.valueOf(movie.getReleaseDate());
+                ps.setDate(9, relaseDate);
+                ps.setString(10, movie.getCast());
+                ps.setString(11, movie.getDirector());
+                ps.setString(12, movie.getAgeRestriction());
+                ps.setString(13, movie.getPoster());
+                return ps;
+            }
+        };
 
-    KeyHolder id = new GeneratedKeyHolder();
-    jdbc.update(psc, id);
+        KeyHolder id = new GeneratedKeyHolder();
+        jdbc.update(psc, id);
 
-    movie.setId(id.getKey().intValue());
-    return movie;
-  }
+        movie.setId(id.getKey().intValue());
+        return movie;
+    }
 
-  public void updateMovie(Movie movie) {
-    jdbc.update("UPDATE movies SET " +
-            "title = ?," +
-            "runtime = ?," +
-            "synopsis = ?, " +
-            "genre = ?, " +
-            "language = ?, " +
-            "subtitles = ?," +
-            "projection_type = ?," +
-            "trailer_link = ?," +
-            "release_date = ?," +
-            "cast = ?," +
-            "director = ?," +
-            "age_restriction = ?," +
-            "poster = ? " +
-            "WHERE id= ?",
-        movie.getTitle(),
-        movie.getRuntime(),
-        movie.getSynopsis(),
-        movie.getGenre(),
-        movie.getLanguage(),
-        movie.getSubtitles(),
-        movie.getProjectionType(),
-        movie.getTrailerLink(),
-        movie.getReleaseDate() == null ? null : Date.valueOf(movie.getReleaseDate()),
-        movie.getCast(),
-        movie.getDirector(),
-        movie.getAgeRestriction(),
-        movie.getPoster(),
-        movie.getId());
-  }
+    public void updateMovie(Movie movie) {
+        jdbc.update("UPDATE movies SET " +
+                        "title = ?," +
+                        "runtime = ?," +
+                        "synopsis = ?, " +
+                        "genre = ?, " +
+                        "language = ?, " +
+                        "subtitles = ?," +
+                        "projection_type = ?," +
+                        "trailer_link = ?," +
+                        "release_date = ?," +
+                        "cast = ?," +
+                        "director = ?," +
+                        "age_restriction = ?," +
+                        "poster = ? " +
+                        "WHERE id= ?",
+                movie.getTitle(),
+                movie.getRuntime(),
+                movie.getSynopsis(),
+                movie.getGenre(),
+                movie.getLanguage(),
+                movie.getSubtitles(),
+                movie.getProjectionType(),
+                movie.getTrailerLink(),
+                movie.getReleaseDate() == null ? null : Date.valueOf(movie.getReleaseDate()),
+                movie.getCast(),
+                movie.getDirector(),
+                movie.getAgeRestriction(),
+                movie.getPoster(),
+                movie.getId());
+    }
 
-  public void deleteMovie(int id) {
-    jdbc.update("DELETE FROM movies WHERE id = ?", id);
-  }
+    public void deleteMovie(int id) {
+        jdbc.update("DELETE FROM movies WHERE id = ?", id);
+    }
 
 }
