7df2a854786b6d90b78a5e7a353811e9c525c7e6
Chris Szabo <szabo.krisztian.87@gmail.com>
2019-05-25 14:14:30 +0200

    Completed docs & comments for Theaters

--- src/main/java/dk/kea/stud/biotrio/cinema/Theater.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/Theater.java
@@ -2,49 +2,99 @@ package dk.kea.stud.biotrio.cinema;
 
 import java.util.List;
 
+/**
+ * The representation of a movie theater within the system
+ */
 public class Theater {
+  /**
+   * An integer representing the theater's id within the database
+   */
   private int id;
+  /**
+   * A string representing the theater's name
+   */
   private String name;
+  /**
+   * An integer representing the number of rows in the theater
+   */
   private int noOfRows;
+  /**
+   * An integer representing the number of seats per row
+   */
   private int seatsPerRow;
+  /**
+   * A list of the theater's supported technologies
+   */
   private List<Technology> supportedTechnologies;
 
+  /**
+   * @return An integer representation of the {@link Theater}'s id
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * @param id An integer to set the {@link Theater}'s id to
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * @return A {@link String} representation of the {@link Theater}'s name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * @param name A {@link String} to set the {@link Theater}'s name to
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * @return An integer representation of the {@link Theater}'s number of rows
+   */
   public int getNoOfRows() {
     return noOfRows;
   }
 
+  /**
+   * @param noOfRows An integer to set the {@link Theater}'s number of rows to
+   */
   public void setNoOfRows(int noOfRows) {
     this.noOfRows = noOfRows;
   }
 
+  /**
+   * @return An integer representation of the {@link Theater}'s number of seats per row
+   */
   public int getSeatsPerRow() {
     return seatsPerRow;
   }
 
+  /**
+   * @param seatsPerRow An integer to set the {@link Theater}'s number of seats per row to
+   */
   public void setSeatsPerRow(int seatsPerRow) {
     this.seatsPerRow = seatsPerRow;
   }
 
+  /**
+   * @return An list of {@link Technology} objects representing the
+   * {@link Theater}'s supported technologies
+   */
   public List<Technology> getSupportedTechnologies() {
     return supportedTechnologies;
   }
 
+  /**
+   * @param supportedTechnologies An list of {@link Technology} objects to set
+   *                              the {@link Theater}'s supported technologies to
+   */
   public void setSupportedTechnologies(List<Technology> supportedTechnologies) {
     this.supportedTechnologies = supportedTechnologies;
   }
--- src/main/java/dk/kea/stud/biotrio/cinema/TheaterRepository.java
+++ src/main/java/dk/kea/stud/biotrio/cinema/TheaterRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Repository class that is responsible with managing {@link Theater} data within the database
+ */
 @Repository
 public class TheaterRepository {
   @Autowired
@@ -15,14 +18,20 @@ public class TheaterRepository {
   @Autowired
   private TechnologyRepository technologyRepo;
 
-  // Find theater by ID
+  /**
+   * Gets a particular theater from the database based on an id
+   *
+   * @param id The integer id to look up in the database
+   * @return A {@link Theater} object if found, otherwise null
+   */
   public Theater findTheater(int id) {
     SqlRowSet rs = jdbc.queryForRowSet("SELECT * FROM theaters WHERE id = ?", id);
-    Theater theater = new Theater();
+    Theater theater = null;
     //while (rs.next()) - reads all the results and then picks the one with the unique id.
     //since the id is unique we know that we get only one result
     // so we use conditioning to read the first (and only row)
     if (rs.first()) {
+      theater = new Theater();
       theater.setId(rs.getInt("id"));
       theater.setName(rs.getString("name"));
       theater.setNoOfRows(rs.getInt("no_of_rows"));
@@ -32,7 +41,11 @@ public class TheaterRepository {
     return theater;
   }
 
-  //List all theaters
+  /**
+   * Gets all the theater records from the database
+   *
+   * @return A list of {@link Theater} objects, or null if there are no records in the database
+   */
   public List<Theater> findAllTheaters() {
     SqlRowSet rs = jdbc.queryForRowSet("SELECT * FROM theaters");
     List<Theater> theaterList = new ArrayList<>();
@@ -49,7 +62,11 @@ public class TheaterRepository {
     return theaterList;
   }
 
-  //Insert a new theater
+  /**
+   * Saves the data of an {@link Theater} object to the database as a new entry
+   *
+   * @param theater The {@link Theater} object to save in the database
+   */
   public void insert(Theater theater) {
     //this works like prepared statement
     jdbc.update("INSERT INTO theaters(name, no_of_rows, seats_per_row) VALUES(?,?,?)",
@@ -57,7 +74,11 @@ public class TheaterRepository {
     technologyRepo.setSupportedTechnologies(theater.getId(), theater.getSupportedTechnologies());
   }
 
-  // Update a theater
+  /**
+   * Update an existing record in the database with the data of an {@link Theater} object
+   *
+   * @param theater A {@link Theater} object to update the database with
+   */
   public void update(Theater theater) {
 
     jdbc.update("UPDATE theaters SET name =? ,no_of_rows =? , seats_per_row =?  WHERE id = ?",
@@ -65,21 +86,32 @@ public class TheaterRepository {
     technologyRepo.setSupportedTechnologies(theater.getId(), theater.getSupportedTechnologies());
   }
 
-  //Delete a theater
+  /**
+   * Deletes a theater entry from the database
+   *
+   * @param id An integer id by which to identify the entry to be deleted
+   */
   public void deleteTheater(int id) {
     technologyRepo.setSupportedTechnologies(id, null);
     jdbc.update("DELETE FROM theaters WHERE id = ?", id);
   }
 
-  // Checks whether a theater can be safely deleted. We want to disallow deleting theaters
-  // that still have associated screenings within the database
+  /**
+   * Checks whether a theater can be safely deleted. We want to disallow deleting theaters
+   * that still have associated screenings within the database
+   *
+   * @param t A {@link Theater} object to check if it's associated record within
+   *          the database can be safely deleted
+   * @return true if it can be safely deleted, otherwise false
+   */
   public boolean canDelete(Theater t){
     String query = ("SELECT COUNT(*) FROM screenings WHERE theater_id= "+t.getId());
     SqlRowSet rs = jdbc.queryForRowSet(query);
     rs.first();
     int noScreenings = rs.getInt(1);
 
-    // If the number of associated screenings is 0, the method will return true
+    // If the number of associated screenings is 0, the theater can be deleted
+    // and thus the method will return true
     return noScreenings == 0;
   }
 
